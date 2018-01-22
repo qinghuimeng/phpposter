287d2504aa5959fc41dddfa5e8924f7df9136519
mengqinghui <2488248789@qq.com>
2018-01-22 16:13:56 +0800

    提交

---
+++ createposter.php
@@ -0,0 +1,15 @@
+<?php 
+function GetPoster($text,$bg_img,$filename){
+	//创建资源
+	$target_img = imagecreatefrompng($bg_img);
+	// 设置字体颜色
+	$textcolor1 =imagecolorexact($target_img, 252,243,187);
+	$TTF ="/poster/msyh.ttf"; 
+    $next_size = 18;
+
+		
+}
+
+
+
+ ?>
